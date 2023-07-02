e7cc245d6201f8e3e1f78993b3bb8cc79083c39f
BiradarAbhijit <apbiradar91@gmail.com>
2023-07-02 13:35:36 +0530

    changes

--- component/Borrowers/records/ViewRecordComponent.js
+++ component/Borrowers/records/ViewRecordComponent.js
@@ -17,7 +17,8 @@ const ViewRecordComponent = ({ records }) => {
     <>
       {details?.id ?
         <>
-        <Typography variant='h5'sx={{backgroundColor : 'aqua', padding: "0.5rem 2rem", borderRadius: "1rem" }} >Borrower Details</Typography>
+        <Typography variant='h5'sx={{backgroundColor : 'aqua', padding: "1rem 2rem", borderRadius: "1rem" }} >Borrower Details</Typography>
+
           {/* <Typography variant='h6' >  Borrower Name : {details?.borrowerName} </Typography>
           <Typography variant='h6' > Due Date : {moment(details?.duePaymentDate).format("MMMM Do YYYY")} </Typography>
           <Typography variant='h6' > Records : {details?.id} </Typography>
@@ -27,18 +28,60 @@ const ViewRecordComponent = ({ records }) => {
           <Typography variant='h6' > ROI : {details?.roi} </Typography>
           <Typography variant='h6' > Total Amount : {details?.totalAmount} </Typography> */}
           
-          <Grid container spacing={2} >
+          <Grid container spacing={2} mt={2}>
             <Grid item xs={12} sx={12} md={6} lg={6} >
                 <Box display={'flex'} >
                     <Typography fontWeight='bold' > Borrower Name :  </Typography>
-                    <Typography  >{details?.borrowerName}  </Typography>
+                    <Typography px={1} >{details?.borrowerName}  </Typography>
                 </Box>
             </Grid>
            
             <Grid item xs={12} sx={12} md={6} lg={6} >
                 <Box display={'flex'} >
                     <Typography fontWeight='bold' >  Due Date :  </Typography>
-                    <Typography  >  {moment(details?.duePaymentDate).format("MMMM Do YYYY")}  </Typography>
+                    <Typography px={1} >  {moment(details?.duePaymentDate).format("MMMM Do YYYY")}  </Typography>
+                </Box>
+            </Grid>
+
+            <Grid item xs={12} sx={12} md={6} lg={6} >
+                <Box display={'flex'} >
+                    <Typography fontWeight='bold' >  Records :  </Typography>
+                    <Typography px={1} >  {details?.id}  </Typography>
+                </Box>
+            </Grid>
+
+            <Grid item xs={12} sx={12} md={6} lg={6} >
+                <Box display={'flex'} >
+                    <Typography fontWeight='bold' >  Interest Amount :  </Typography>
+                    <Typography px={1} >  {details?.interestAmount.toFixed(2)}  </Typography>
+                </Box>
+            </Grid>
+
+            <Grid item xs={12} sx={12} md={6} lg={6} >
+                <Box display={'flex'} >
+                    <Typography fontWeight='bold' >  Principal Amount :  </Typography>
+                    <Typography px={1} >  {details?.principalAmount}  </Typography>
+                </Box>
+            </Grid>
+
+            <Grid item xs={12} sx={12} md={6} lg={6} >
+                <Box display={'flex'} >
+                    <Typography fontWeight='bold' >  Purchase Date :  </Typography>
+                    <Typography px={1} >  {moment(details?.purchaseDate).format("MMMM Do YYYY")}  </Typography>
+                </Box>
+            </Grid>
+
+            <Grid item xs={12} sx={12} md={6} lg={6} >
+                <Box display={'flex'} >
+                    <Typography fontWeight='bold' >  ROI :  </Typography>
+                    <Typography px={1} >  {details?.roi}  </Typography>
+                </Box>
+            </Grid>
+
+            <Grid item xs={12} sx={12} md={6} lg={6} >
+                <Box display={'flex'} >
+                    <Typography fontWeight='bold' >  Total Amount :  </Typography>
+                    <Typography px={1} >  {details?.totalAmount.toFixed(2)}  </Typography>
                 </Box>
             </Grid>
           </Grid>
